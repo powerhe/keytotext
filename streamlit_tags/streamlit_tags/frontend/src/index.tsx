e91523922397f4be9d649b97ed83990f25f6afb1
Li.HE <576975011@qq.com>
2022-09-14 16:24:40 +0800

    Add files via upload

---
+++ streamlit_tags/streamlit_tags/frontend/src/index.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Custom_keywords from "./keywords"
+
+// Lots of import to define a Styletron engine and load the light theme of baseui
+// @ts-ignore
+import { Client as Styletron } from "styletron-engine-atomic"
+import { Provider as StyletronProvider } from "styletron-react"
+import { ThemeProvider, LightTheme } from "baseui"
+
+const engine = new Styletron()
+
+// Wrap your CustomSlider with the baseui them
+ReactDOM.render(
+  <React.StrictMode>
+    <StyletronProvider value={engine}>
+      <ThemeProvider theme={LightTheme}>
+        <Custom_keywords />
+      </ThemeProvider>
+    </StyletronProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
---
+++ streamlit_tags/streamlit_tags/frontend/src/keywords.tsx
@@ -0,0 +1,39 @@
+import React,{ useEffect, useState }  from "react"
+import { ComponentProps, Streamlit, withStreamlitConnection } from "streamlit-component-lib"
+import { TagsInput } from "./react-tag-input-componet";
+import "./styles.css";
+
+interface PythonArgs {
+  label: string
+  text: string
+  initialValue: string[]
+  suggestions: string[]
+  maxTags: number
+}
+
+const Custom_keywords = (props: ComponentProps) => {
+  // Destructure using Typescript interface
+  // This ensures typing validation for received props from Python
+  let { label, text, initialValue, suggestions, maxTags}: PythonArgs = props.args
+  const [value, setValue] = useState(initialValue)
+
+  const onSubmit = (values: string[]) => {
+    setValue(values)
+    Streamlit.setComponentValue((values))
+  }
+  useEffect(() => Streamlit.setFrameHeight())
+  return (
+    <div>
+        <TagsInput
+        value={value}
+        onChange= {(value) =>  onSubmit(value)}
+        name={label}
+        placeHolder={text}
+        suggestions={suggestions}
+        maxTags={maxTags}
+      />
+    </div>
+  )
+}
+
+export default withStreamlitConnection(Custom_keywords)
