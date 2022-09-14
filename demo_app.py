94184f2cc2bc870d78ebe05079b514536b56d30c
Li.HE <576975011@qq.com>
2022-09-14 19:37:07 +0800

    Update demo_app.py

--- demo_app.py
+++ demo_app.py
@@ -16,8 +16,9 @@ st.write("# Code for Keywords to Text")
 #    key='1')''',
 #        language="python")
 
-st.text('Idea is to build a model which will take keywords as inputs and generate sentences as outputs.')
+st.text('## Idea is to build a model which will take keywords as inputs and generate sentences as outputs.')
         
+st.sidebar.write("# Parameter Selection")
 maxtags_sidebar = st.sidebar.slider('Number of tags allowed?', 1, 10, 3, key='ehikwegrjifbwreuk')
 keywords = st_tags(
     label='# Enter Keywords:',
@@ -32,10 +33,9 @@ st.write("## Results:")
 ##########
 ## sidebar
 ##########
-st.sidebar.write("# Parameter Selection")
 
 # Add selectbox in streamlit
-option = st.selectbox(
+option = st.sidebar.selectbox(
      'Which model would you like to be selected?',
      ('k2t', 'k2t-base', 'mrm8488/t5-base-finetuned-common_gen'))
 
